3f3ef7e5921ac3666902c1a773139bbc7930d88a
imxieke <oss@live.hk>
2024-05-06 00:32:50 +0800

    update php82 and anmp.compose.yml

--- dockenv.sh
+++ dockenv.sh
@@ -1,73 +1,85 @@
 #!/usr/bin/env bash
+###
+# @Author: Cloudflying
+# @Date: 2024-04-30 18:51:36
+# @LastEditTime: 2024-05-01 22:06:32
+# @LastEditors: Cloudflying
+# @Description:
+###
 # Docker Compose Manager
 
 ROOT_PATH=$(dirname "$(realpath "$0")")
 
 _start() {
-    "$@" up -d
+  "$@" up -d
 }
 
 _stop() {
-    "$@" stop
+  "$@" stop
 }
 
 _restart() {
-    "$@" restart
+  "$@" restart
 }
 
 _remove() {
-    "$@" down
+  "$@" down
 }
 
 _recreate() {
-    "$@" up --force-recreate -d
+  "$@" up --force-recreate -d
 }
 
 _ps() {
-    "$@" ps
+  "$@" ps
 }
 
-_service()
-{
-    COMPOSE_FILE="${ROOT_PATH}/compose/${2}.compose.yml"
-    [ -z "${2}" ] && echo "error: Service is empty" && exit 1
-
-    if [[ -f "${COMPOSE_FILE}" ]]; then
-        # echo "==> $1 $2"
-        "_${1}" docker-compose -f "${COMPOSE_FILE}" --env-file "${ROOT_PATH}/.env" --project-name "${2}"
-    else
-        echo "error: Service $2 Not Found, See ./dockenv.sh -h"
-    fi
+_pull() {
+  "$@" pull
 }
 
-_usage()
-{
-    echo "Usage: bash $0 [start|stop|restart|recreate|remove] [anmp|elk|services|gitea|downloader|mariadb]"
+_service() {
+  COMPOSE_FILE="${ROOT_PATH}/compose/${2}.compose.yml"
+  [ -z "${2}" ] && echo "error: Service is empty" && exit 1
+
+  if [[ -f "${COMPOSE_FILE}" ]]; then
+    # echo "==> $1 $2"
+    "_${1}" docker-compose -f "${COMPOSE_FILE}" --env-file "${ROOT_PATH}/.env" --project-name "${2}"
+  else
+    echo "error: Service $2 Not Found, See ./dockenv.sh -h"
+  fi
+}
+
+_usage() {
+  echo "Usage: bash $0 [start|stop|restart|recreate|remove] [anmp|elk|services|gitea|downloader|mariadb]"
 }
 
 case "$1" in
-    start)
-        _service "$@"
-        ;;
-    stop)
-        _service "$@"
-        ;;
-    restart)
-        _service "$@"
-        ;;
-    recreate)
-        _service "$@"
-        ;;
-    remove)
-        _service "$@"
-        ;;
-    ps)
-        _service "$@"
-        ;;
-    "-h|--help|usage|--usage")
-        _usage
-        ;;
-    *)
-        _usage
-        ;;
+start)
+  _service "$@"
+  ;;
+stop)
+  _service "$@"
+  ;;
+restart)
+  _service "$@"
+  ;;
+recreate)
+  _service "$@"
+  ;;
+remove)
+  _service "$@"
+  ;;
+ps)
+  _service "$@"
+  ;;
+pull)
+  _service "$@"
+  ;;
+"-h|--help|usage|--usage")
+  _usage
+  ;;
+*)
+  _usage
+  ;;
 esac
