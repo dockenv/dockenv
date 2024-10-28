26e52a89b93c269b735d48b6247c8a1bc7ca7716
imxieke <oss@live.hk>
2024-10-28 17:03:02 +0800

    add compose secondary command parameters

--- dockenv.sh
+++ dockenv.sh
@@ -2,8 +2,8 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-04-30 18:51:36
- # @LastEditTime: 2024-10-28 10:23:26
- # @LastEditors: Cloudflying
+# @LastEditTime: 2024-10-28 13:54:37
+# @LastEditors: Cloudflying
 # @Description: Dockenv Manager
 ###
 
@@ -20,12 +20,6 @@ if [[ -z ${DOCKENV_NAME} ]]; then
   exit 1
 fi
 
-COMPOSE_CMD=$2
-
-if [[ -z "${COMPOSE_CMD}" ]]; then
-  COMPOSE_CMD="--help"
-fi
-
 COMPOSE_FILE="${ROOT_PATH}/.dev/compose/${DOCKENV_NAME}.compose.yml"
 COMPOSE_OPTS="--env-file ${ROOT_PATH}/.env --progress=tty --project-name=${DOCKENV_NAME} -f ${COMPOSE_FILE}"
 
@@ -34,9 +28,7 @@ if [[ ! -f "${COMPOSE_FILE}" ]]; then
   exit 1
 fi
 
-__exec()
-{
-  docker-compose ${COMPOSE_OPTS} ${COMPOSE_CMD}
-}
+# Remove first Command Line Options
+shift
 
-__exec
+docker-compose ${COMPOSE_OPTS} "$@"
