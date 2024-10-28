fb3c0fb7689b5b83b6549ae3c5ee15bccd5729bc
imxieke <oss@live.hk>
2024-10-28 10:23:49 +0800

    update dockenv cmd

--- dockenv.sh
+++ dockenv.sh
@@ -2,11 +2,10 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-04-30 18:51:36
-# @LastEditTime: 2024-07-22 11:42:01
-# @LastEditors: Cloudflying
-# @Description: Dockenv Compose Manager
+ # @LastEditTime: 2024-10-28 10:23:26
+ # @LastEditors: Cloudflying
+# @Description: Dockenv Manager
 ###
-# Docker Compose Manager
 
 ROOT_PATH=$(dirname "$(realpath "$0")")
 
@@ -14,75 +13,30 @@ if [[ ! -f "${ROOT_PATH}/.env" ]]; then
   cp -fr "${ROOT_PATH}/.env-example" "${ROOT_PATH}/.env"
 fi
 
-_start() {
-  "$@" up -d
+DOCKENV_NAME=$1
+
+if [[ -z ${DOCKENV_NAME} ]]; then
+  echo "Error: No dockenv specified."
+  exit 1
+fi
+
+COMPOSE_CMD=$2
+
+if [[ -z "${COMPOSE_CMD}" ]]; then
+  COMPOSE_CMD="--help"
+fi
+
+COMPOSE_FILE="${ROOT_PATH}/.dev/compose/${DOCKENV_NAME}.compose.yml"
+COMPOSE_OPTS="--env-file ${ROOT_PATH}/.env --progress=tty --project-name=${DOCKENV_NAME} -f ${COMPOSE_FILE}"
+
+if [[ ! -f "${COMPOSE_FILE}" ]]; then
+  echo "error: Service $2 Not Found, See ./dockenv.sh -h"
+  exit 1
+fi
+
+__exec()
+{
+  docker-compose ${COMPOSE_OPTS} ${COMPOSE_CMD}
 }
 
-_stop() {
-  "$@" stop
-}
-
-_restart() {
-  "$@" restart
-}
-
-_remove() {
-  "$@" down
-}
-
-_recreate() {
-  "$@" up --force-recreate -d
-}
-
-_ps() {
-  "$@" ps
-}
-
-_pull() {
-  "$@" pull
-}
-
-_service() {
-  COMPOSE_FILE="${ROOT_PATH}/.dev/compose/${2}.compose.yml"
-  [ -z "${2}" ] && echo "error: Service is empty" && exit 1
-
-  if [[ -f "${COMPOSE_FILE}" ]]; then
-    "_${1}" docker-compose -f "${COMPOSE_FILE}" --env-file "${ROOT_PATH}/.env" --project-name "${2}"
-  else
-    echo "error: Service $2 Not Found, See ./dockenv.sh -h"
-  fi
-}
-
-_usage() {
-  echo "Usage: bash $0 [start|stop|restart|recreate|remove] [anmp|elk|services|gitea|downloader|mariadb]"
-}
-
-case "$1" in
-start)
-  _service "$@"
-  ;;
-stop)
-  _service "$@"
-  ;;
-restart)
-  _service "$@"
-  ;;
-recreate)
-  _service "$@"
-  ;;
-remove)
-  _service "$@"
-  ;;
-ps)
-  _service "$@"
-  ;;
-pull)
-  _service "$@"
-  ;;
-"-h|--help|usage|--usage")
-  _usage
-  ;;
-*)
-  _usage
-  ;;
-esac
+__exec
