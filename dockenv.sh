abe695f0ff3818570aa591ebe897653b5f173bb3
imxieke <oss@live.hk>
2025-03-18 17:03:34 +0800

    update .env

--- dockenv.sh
+++ dockenv.sh
@@ -2,7 +2,7 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-04-30 18:51:36
-# @LastEditTime: 2024-10-28 13:54:37
+# @LastEditTime: 2025-03-18 16:53:34
 # @LastEditors: Cloudflying
 # @Description: Dockenv Manager
 ###
@@ -10,7 +10,7 @@
 ROOT_PATH=$(dirname "$(realpath "$0")")
 
 if [[ ! -f "${ROOT_PATH}/.env" ]]; then
-  cp -fr "${ROOT_PATH}/.env-example" "${ROOT_PATH}/.env"
+  cp -fr "${ROOT_PATH}/.example.env" "${ROOT_PATH}/.env"
 fi
 
 DOCKENV_NAME=$1
@@ -23,7 +23,7 @@ fi
 COMPOSE_FILE="${ROOT_PATH}/.dev/compose/${DOCKENV_NAME}.compose.yml"
 COMPOSE_OPTS="--env-file ${ROOT_PATH}/.env --progress=tty --project-name=${DOCKENV_NAME} -f ${COMPOSE_FILE}"
 
-if [[ ! -f "${COMPOSE_FILE}" ]]; then
+if [[ ! -f ${COMPOSE_FILE} ]]; then
   echo "error: Service $2 Not Found, See ./dockenv.sh -h"
   exit 1
 fi
